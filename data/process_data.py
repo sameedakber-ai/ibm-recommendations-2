b713c6b67f41e16313b2109d9f1ec5105616b721
Sameed Akber <sameedakber.ai@gmail.com>
2019-10-08 16:28:44 -0400

    fix: Extract links from subset of entire data

--- data/process_data.py
+++ data/process_data.py
@@ -24,9 +24,9 @@ df = df.astype({'article_id': int})
 user_id_dict = dict()
 i=0
 for email in df.email:
-	if email not in user_id_dict:
-		user_id_dict[email] = i
-		i+=1
+    if email not in user_id_dict:
+        user_id_dict[email] = i
+        i+=1
 df['user_id'] = df.email.apply(lambda x: user_id_dict[x])
 df.drop('email', axis=1, inplace=True)
 
@@ -40,19 +40,23 @@ df_merged = df.drop('title', axis=1).merge(df_content[['article_id', 'doc_full_n
 # Fill in missing document titles
 no_title_ids = df_merged.article_id[df_merged.doc_full_name.isnull()].unique().tolist()
 for id in no_title_ids:
-	title = df.title[df.article_id == id].tolist()[0]
-	df_merged.doc_full_name[df_merged.article_id == id] = title
+    title = df.title[df.article_id == id].tolist()[0]
+    df_merged.doc_full_name[df_merged.article_id == id] = title
 df_merged.doc_description[df_merged.doc_description.isnull()] = ''
 
+df_subset = df_merged[['article_id', 'doc_full_name', 'doc_description']].drop_duplicates(keep='first')
+
 # Extract article links through google searches
-doc_identifier = df_merged.doc_full_name + ' ' + df_merged.doc_description
+doc_identifier = df_subset.doc_full_name + ' ' + df_subset.doc_description
 def extract_link(text):
-	try:
-		link = list(search(text, tld="com", num=2, stop=2))[0]
-	except:
-		link = "{{ url_for('notfound') }}"
-	return link
-df_merged['link'] = doc_identifier.progress_apply(extract_link)
+    try:
+        link = list(search(text, tld="com", num=1, stop=1))[0]
+    except:
+        link = "{{ url_for('notfound') }}"
+    return link
+df_subset['link'] = doc_identifier.progress_apply(extract_link)
+
+df_merged['link'] = df_merged.article_id.apply(lambda x: df_subset.link[df_subset.article_id==x].tolist()[0])
 
 # Save data to database
 engine = create_engine('sqlite:///data/data.db')
