b8c18ae4ef4fefffd5ad6b56a4c3cc18418fd475
Sameed Akber <sameedakber.ai@gmail.com>
2019-10-07 21:03:50 -0400

    feat: Add recommendations display functionality

--- app.py
+++ app.py
@@ -17,20 +17,26 @@ class User(db.Model):
 
 
 @app.route('/')
-def hello_world():
+def helloK_world():
     return render_template('index.html')
 
 
 @app.route('/signin', methods=['POST', 'GET'])
 def signin():
     if request.method == 'POST':
-        user_id = request.form['userid']
+        user_id = int(request.form['userid'])
         if user_id in df.user_id.tolist():
-            recs = Collaborative(df, user_id)
+            recommender = Collaborative(df, user_id)
+            recs = recommender.make_collaborative_recs(15)
             return render_template('user.html', user_id=user_id, recs=recs)
         else:
             return render_template('newuser.html', user_id=user_id)
 
+@app.route('/article/<int:articleid>')
+def show_article_id(article_id):
+    if article_id in df.article_id:
+        article_content = df[['article_id', 'doc_body']]
+        return render_template('article.html', )
 
 
 
