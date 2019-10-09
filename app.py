31cb437067ab6522282e799c703e206d6b336a41
Sameed Akber <sameedakber.ai@gmail.com>
2019-10-09 15:02:32 -0400

    feat: Add POST route to update user database

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+from datetime import  datetime
 
 from model.recommender import *
 
@@ -12,37 +14,50 @@ class User(db.Model):
     time = db.Column(db.DateTime, primary_key=True)
     article = db.Column(db.String(150))
 
-    def __repr__(self):
-        return '<User %r>' % self.id
-
+    def __init__(self, id, time, article):
+        self.id = id
+        self.time = time
+        self.article = article
 
 @app.route('/')
-def helloK_world():
+def hello_world():
     return render_template('index.html')
 
-
-@app.route('/signin', methods=['POST', 'GET'])
-def signin():
+@app.route('/temp', methods=['POST', 'GET'])
+def temp():
     if request.method == 'POST':
         user_id = int(request.form['userid'])
         if user_id in df.user_id.tolist():
-            recommender = Collaborative(df, user_id)
-            recs = recommender.make_collaborative_recs(15)
-            return render_template('user.html', user_id=user_id, recs=recs)
-        else:
-            return render_template('newuser.html', user_id=user_id)
+            return redirect(url_for('welcomeuser', userid=user_id))
 
-@app.route('/article/<int:articleid>')
-def show_article_id(article_id):
-    if article_id in df.article_id:
-        article_content = df[['article_id', 'doc_body']]
-        return render_template('article.html', )
+@app.route('/user-<int:userid>', methods=['POST', 'GET'])
+def welcomeuser(userid):
+        recommender = Collaborative(df, userid)
+        recs = recommender.make_collaborative_recs(15)
+        recent = User.query.filter_by(id=int(userid)).order_by(desc(User.time)).all()
+        rec_art = [rec.article for rec in recent]
+        rec_time = [rec.time for rec in recent]
+        rec_link = df.link[df.doc_full_name.isin(rec_art)].drop_duplicates(keep='first').tolist()
+        result = list(zip(rec_art, rec_time, rec_link))[:5]
+        return render_template('user.html', user_id=userid, recs=recs, result=result)
 
 
-@app.route('/notfound')
-def notfound():
-    return render_template('lost.html')
+@app.route('/<path:subpath>-<int:id>-<article>', methods=['GET', 'POST'])
+def updatedatabase(subpath, id, article):
+    global df
+    if article not in df.doc_full_name[df.user_id==id].tolist():
+        df = df.append({'user_id': id, 'doc_full_name': article}, ignore_index=True)
+    user = User.query.filter_by(id=id).all()
+    if article not in [data.article for data in user]:
+        time = datetime.now()
+        new_entry = User(int(id), time, article)
+        db.session.add(new_entry)
+        db.session.commit()
+    else:
+        User.query.filter_by(id=id, article=article).update(dict(time = datetime.now()))
+        db.session.commit()
 
+    return redirect(subpath)
 
 if __name__ == '__main__':
     app.run(debug=True)
